429ade303defa5e2f48f4abff87a5b3cd628c3c2
seunghyun1003 <58840682+seunghyun1003@users.noreply.github.com>
2020-01-24 23:53:53 +0900

    Update 카펫.py

--- LV2/카펫.py
+++ LV2/카펫.py
@@ -1,7 +1,5 @@
 def solution(brown, red):
-    answer = [red+2, 3]
-    for i in range(1, red//2):
+    for i in range(1, red//2+1): #red가 1일경우 red//2는 0이 되므로 +1해준다.
         if red % i == 0:
             if brown == (i+red//i+2)*2:
-                answer = [red//i+2, i+2]
-    return answer
+                return [red//i+2, i+2]
