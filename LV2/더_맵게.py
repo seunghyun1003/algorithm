837d6072887cd443b843e82dfb61fface1d2da7a
seunghyun1003 <58840682+seunghyun1003@users.noreply.github.com>
2020-01-26 23:53:48 +0900

    Update 더_맵게.py

--- LV2/더_맵게.py
+++ LV2/더_맵게.py
@@ -14,4 +14,21 @@ def solution(scoville, K):
         else:
             return answer
             
-//
+//리스트의 최솟값을 이용하는 문제이므로 heapq을 이용하였다.
+import heapq
+
+def solution(scoville,K):
+    answer = 0
+    scoville_heap = []
+    for i in scoville:  //힙에 scoville의 모든 요소 넣기
+        heapq.heappush(scoville_heap,i)
+    while(1):
+        if scoville_heap[0]>=K:
+            return answer
+        elif len(scoville_heap)==1:
+            return -1
+        else:
+            Min = heapq.heappop(scoville_heap)
+            Min2 = heapq.heappop(scoville_heap)
+            heapq.heappush(scoville_heap, Min+Min2*2)
+            answer += 1
